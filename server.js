5e80ee07b8b4e5e866f0fd859c193c62d96edbd6
Jonas Pommerening <jonas.pommerening@gmail.com>
2015-04-02 17:54:44 +0200

    Add webhook -> sse event fanout and debug console

---
+++ server.js
@@ -0,0 +1,43 @@
+var restify = require( 'restify' );
+var debug = require( 'debug' )( 'http' );
+var fs = require( 'fs' );
+
+var package = require( './package.json' );
+var middleware = require( './lib/middleware' );
+
+var server = restify.createServer( {
+   name: package.name,
+   version: package.version,
+   acceptable: [ 'text/event-stream' ]
+} );
+
+//server.use( restify.acceptParser( server.acceptable ) );
+server.use( restify.queryParser() );
+server.use( restify.bodyParser() );
+
+server.pre( function( req, res, next ) {
+   req.log.info( '%s %s', req.method, req.url );
+   next();
+} );
+
+server.get( '/console', function( req, res, next ) {
+   res.setHeader( 'Content-Type', 'text/html' );
+   fs.createReadStream( 'static/index.html' ).pipe( res );
+} );
+server.get( '/api', middleware.api );
+server.get( '/sse', middleware.sse );
+server.post( '/webhook', middleware.webhook );
+
+/*
+server.use( restify.conditionalRequest() );
+server.use( restify.gzipResponse() );
+server.use( restify.CORS( {
+   origins: [ '*' ]
+} ) );
+*/
+
+server.listen( 8080, function() {
+   server.log.info( '%s listening %s', server.name, server.url );
+} );
+
+server.log.level('TRACE');
